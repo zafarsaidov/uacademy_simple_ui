8fee3edcd4754a0cc11a824a84566913f7fb1bb1
z.saidov <z.saidov@proximaops.io>
2024-06-03 21:07:05 +0500

    Initial

--- main.py
+++ main.py
@@ -1,46 +1,20 @@
 from flask import Flask, request, jsonify
 from dotenv import load_dotenv
 import os
+import requests
 
-load_dotenv("/var/devops/flask_app/.env")
+load_dotenv("/var/devops/ui/.env")
 
 app = Flask(__name__)
-postgres_host = os.getenv("POSTGRES_HOST", "localhost")
-postgres_port = os.getenv("POSTGRES_PORT", "5432")
-postgres_user = os.getenv("POSTGRES_USER", "postgres")
-postgres_password = os.getenv("POSTGRES_PASSWORD", "postgres")
-postgres_db = os.getenv("POSTGRES_DB", "postgres")
+
 application_host = os.getenv("APPLICATION_HOST", "0.0.0.0")
 application_port = os.getenv("APPLICATION_PORT", 8888)
+backend_api_url = os.getenv("BACKEND_API_URL", "http://0.0.0.0:8080")
 
-
-@app.route("/api/v1/user/sum", methods=["POST"])
+@app.route("/get", methods=["GET"])
 def start():
-    a = request.json.get("a", "")
-    b = request.json.get("b", "")
-    d = {
-        "a": a,
-        "b": b
-    }
+    response = requests.get(backend_api_url + "/getdb")
+    d = response.json()
     return jsonify(d)
-
-@app.route("/getdb", methods=["GET"])
-def getversion():
-    response = {
-        "postgres_host": postgres_host, 
-        "postgres_port": postgres_port,
-        "postgres_user": postgres_user,
-        "postgres_password": postgres_password,
-        "postgres_db": postgres_db
-    }
-    return jsonify(response)
-
-@app.route("/hi")
-def hi():
-    return "Hi"
-
-@app.route("/bye")
-def bye():
-    return "Bye"
 
 app.run(host=application_host, port=application_port)
